ef1287ac1f9d9c099342fb824327d11da96272b5
4javier <4javier4@gmail.com>
2023-01-06 18:17:43 +0100

    working radio group

--- src/app/directives/selectable-directive-material.directive.ts
+++ src/app/directives/selectable-directive-material.directive.ts
@@ -1,5 +1,5 @@
-import { Directive, Renderer2, ViewContainerRef } from '@angular/core';
-import { NgControl } from '@angular/forms';
+import { ContentChild, Directive, ElementRef, inject, ViewContainerRef } from '@angular/core';
+import { FormControlName, NgControl } from '@angular/forms';
 import { MatCheckbox } from '@angular/material/checkbox'; 
 
 @Directive({
@@ -8,20 +8,32 @@ import { MatCheckbox } from '@angular/material/checkbox';
 })
 export class SelectableDirectiveMaterialDirective {
 
+  @ContentChild(FormControlName, {read: NgControl}) ctrl!: NgControl;
+  _radioCtrl?: NgControl;
+  
   constructor(
-    private renderer: Renderer2,
-    private vcr: ViewContainerRef,
-    private ctrl: NgControl) {}
-
+      private vcr: ViewContainerRef,
+      private el: ElementRef,
+  ) {
+    if(this.el.nativeElement.nodeName === 'MAT-RADIO-GROUP') {
+      this._radioCtrl = inject(NgControl);
+    }
+  }
+  
   ngAfterViewInit() {
+
+    if(this.el.nativeElement.nodeName === 'MAT-RADIO-GROUP') {
+      this.ctrl = this._radioCtrl!;
+    }
     
     const matCheckBox = this.vcr.createComponent(MatCheckbox)
-    this.renderer.setStyle(matCheckBox.instance._elementRef.nativeElement, 'pointer-events', 'auto')
+  //  this.renderer.setStyle(matCheckBox.instance._elementRef.nativeElement, 'pointer-events', 'auto')
     if (this.ctrl.control?.enabled) 
       matCheckBox.setInput('checked', true);
     
     matCheckBox.instance.change.subscribe(
       v => v.checked ? this.ctrl.control?.enable() : this.ctrl.control?.disable()
     );
+
   }
 }
