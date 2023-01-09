822a7b66f7a593858b54754fc4768239eca5d18c
4javier <4javier4@gmail.com>
2023-01-09 12:56:26 +0100

    renamed mat directive

--- src/app/directives/selectable-directive-material.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SelectableDirectiveMaterialDirective } from './selectable-directive-material.directive';
-
-describe('SelectableDirectiveMaterialDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SelectableDirectiveMaterialDirective();
-    expect(directive).toBeTruthy();
-  });
-});
---
+++ src/app/directives/selectable-material.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SelectableMaterialDirective } from './selectable-material.directive';
+
+describe('SelectableMaterialDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SelectableMaterialDirective();
+    expect(directive).toBeTruthy();
+  });
+});
